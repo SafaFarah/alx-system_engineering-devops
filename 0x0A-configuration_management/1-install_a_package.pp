a5b06fab4aa9dcc38de7101871a74ecf41bd569f
Safa Farah <kangjisook13@gmail.com>
2024-01-21 22:59:26 +0200

    0x0B-ssh task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,14 @@
 #Using Puppet, install flask from pip3.
 #Install flask
 #Version must be 2.1.0
+package { 'werkzeung':
+    ensure   => '2.2.2',
+    provider => 'pip3',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['werkzeung'],
 }
+
