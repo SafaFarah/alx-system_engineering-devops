65f0c17454102c455a6a92c169a931c4d94232f1
SafaFarah <kangjisook13@gmail.com>
2024-03-29 14:42:57 +0200

    debug tasks

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,16 +1,17 @@
-#config server
+# Install Nginx web server (w/ Puppet) 
 
 package { 'nginx':
 provider => 'apt',
 }
-exec {'hlbtn_page':
+
+exec {'GET_request':
 command => '/usr/bin/sudo /bin/echo Hello World! > /var/www/html/index.nginx-debian.html',
 }
-exec {'redirect_page':
 
+exec {'redirect_me':
 command => '/usr/bin/sudo /bin/sed -i "66i rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
 }
-exec {'start_server':
 
+exec {'start_nginx':
 command => '/usr/bin/sudo /usr/sbin/service nginx start',
 }
