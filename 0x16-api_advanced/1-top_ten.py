2ab849c7185f6f338db5300be0c6ec4755b4d301
SafaFarah <kangjisook13@gmail.com>
2024-07-18 07:13:45 +0300

    debug 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ import requests
 
 def top_ten(subreddit):
     """Function that prints the titles of the first 10 hot posts."""
-        url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
