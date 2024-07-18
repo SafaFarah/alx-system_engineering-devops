1d2dc154d4d8bc9cce36c97630bd8950a0f72b7a
SafaFarah <kangjisook13@gmail.com>
2024-07-18 07:05:18 +0300

    debug 1-top_ten.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """a function queries  Reddit API and returns the number of subscribers """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'CustomClient/1.0'}
+    headers = {"User-Agent": "Mozilla/5.0"}
     results = requests.get(url, headers=headers, allow_redirects=False)
     if results.status_code == 200:
         data = results.json()
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,16 +8,15 @@ import requests
 
 def top_ten(subreddit):
     """Function that prints the titles of the first 10 hot posts."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
-            for post in posts:
-                print(post['data']['title'])
-        else:
-            print(None)
-    except requests.RequestException:
-        print(None)
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
